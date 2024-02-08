7796a42b158e3442a70858d428883f8be7d05788
emindemirhan <emindemirhan@icloud.com>
2024-02-08 18:45:04 +0300

    Update App component and font styles, add folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 function App(): JSX.Element {
-  return <div className="text-red-500">test</div>;
+  return (
+    <div className="text-red-500">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque,
+      dolorem ex nemo eveniet magni error, alias possimus tempora velit numquam
+      odio ipsa officia recusandae eos voluptatem quas suscipit aspernatur
+      deleniti?
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,14 @@ module.exports = {
         gradient2: "linear-gradient(180deg, #cf5ec9 0%, #2347ad 82%)",
       },
     },
+    screens: {
+      xs: "428px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 };
